91e8056b7ebdfe774294dfe5f455f12acf432b95
Evgeny Zemskov <ezemskov@yandex.ru>
2020-03-09 21:32:53 +1100

    Simplest json config and region filtering added, take 2

--- src/ausnet_to_gpx.py
+++ src/ausnet_to_gpx.py
@@ -1,26 +1,48 @@
+import json
 import gpxpy
 import gpxpy.gpx
 import polyline
 import csv
 
+def inAnyOfRegions(vertexTuple, regionsArray):
+    vertexLat, vertexLon = vertexTuple
+
+    for region in regionsArray : 
+        if ((vertexLat > region["south"]) and 
+            (vertexLat < region["north"]) and
+            (vertexLon > region["west"]) and
+            (vertexLon < region["east"])) : 
+            return True;
+    
+    return False;
+
+ConfigFilename = 'ausnet_to_gpx.json';
+with open(ConfigFilename, newline='') as jsonFile:
+    jsonStr = jsonFile.read();
+    jsonDic = json.loads(jsonStr);
+    regionsArrayCfg = jsonDic["include_regions"];
+
 gpx = gpxpy.gpx.GPX()
-gpx_track = gpxpy.gpx.GPXTrack()
-gpx.tracks.append(gpx_track)
+gpxTrack = gpxpy.gpx.GPXTrack()
+gpx.tracks.append(gpxTrack)
 
-#ausnetFilename = 'data/12kV_Ausnet_Victorian_Transmission_MV_Lines.txt';
-ausnetFilename = 'data/22kV_Ausnet_Victorian_Sub-Transmission_MV_Lines.txt';
+#AusnetFilename = '../data/12kV_Ausnet_Victorian_Transmission_MV_Lines.txt';
+AusnetFilename = '../data/22kV_Ausnet_Victorian_Sub-Transmission_MV_Lines.txt';
 
-with open(ausnetFilename, newline='') as csvfile:
+with open(AusnetFilename, newline='') as csvfile:
     ausnetReader = csv.reader(csvfile)
     for row in ausnetReader:
-        gpx_segment = gpxpy.gpx.GPXTrackSegment()
-        gpx_track.segments.append(gpx_segment)
+        gpxSegment = gpxpy.gpx.GPXTrackSegment()
 
         polylineEnc = row[1] 
         polylineDec = polyline.decode(polylineEnc)
-        for vertex_tuple in polylineDec:
-            vertex_lat, vertex_lon = vertex_tuple
-            gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(vertex_lat, vertex_lon, elevation=0))
+        for vertexTuple in polylineDec:
+            if inAnyOfRegions(vertexTuple, regionsArrayCfg) :
+                vertexLat, vertexLon = vertexTuple
+                gpxSegment.points.append(gpxpy.gpx.GPXTrackPoint(vertexLat, vertexLon, elevation=0))
+
+        if len(gpxSegment.points) > 0:
+            gpxTrack.segments.append(gpxSegment)    
 
 gpxFile = open("ausnet_res.gpx", "w")
 gpxFile.write(gpx.to_xml());
